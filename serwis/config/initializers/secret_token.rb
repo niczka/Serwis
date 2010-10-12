83323c6946669795948acee0bd802fc53bd88465
niczka <dominikarogozinska@gmail.com>
2010-10-12 23:32:46 +0200

    utworzono nową aplikację

---
+++ serwis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serwis::Application.config.secret_token = 'c6abb17833ac5922e9da015be70354f1743980f39368c29b0428ff6f339691a7dabf57f0eff64522fa3ad42ef5e2dcf7d413177225299536806fb0647c20658d'
